d575d9cd2a01a682dfba422f4cad82df8a4320f9
Josip Marković <josip.markovic@factory.hr>
2019-09-19 14:37:25 +0200

    Initial setup

---
+++ MyCustomPod/MyCustomPod.h
@@ -0,0 +1,19 @@
+//
+//  MyCustomPod.h
+//  MyCustomPod
+//
+//  Created by Josip Marković on 19/09/2019.
+//  Copyright © 2019 Josip Marković. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyCustomPod.
+FOUNDATION_EXPORT double MyCustomPodVersionNumber;
+
+//! Project version string for MyCustomPod.
+FOUNDATION_EXPORT const unsigned char MyCustomPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyCustomPod/PublicHeader.h>
+
+
---
+++ MyPodExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPodExample
+//
+//  Created by Josip Marković on 19/09/2019.
+//  Copyright © 2019 Josip Marković. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
