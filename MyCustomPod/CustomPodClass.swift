329abe90424d4d82a2e9b9da00132e1a7092cbfb
Josip Marković <josip.markovic@factory.hr>
2019-09-19 14:42:34 +0200

    Added basic function to pod

---
+++ MyCustomPod/CustomPodClass.swift
@@ -0,0 +1,20 @@
+//
+//  CustomPodClass.swift
+//  MyCustomPod
+//
+//  Created by Josip Marković on 19/09/2019.
+//  Copyright © 2019 Josip Marković. All rights reserved.
+//
+
+import Foundation
+
+
+public final class CustomPodClass{
+    
+    
+    let name: String = "Pod class"
+    
+    func giveMeName() -> String{
+        return name
+    }
+}
